1b26ab40ed29519b382ecd3b7eed1619b2e8ef37
joy.deng <joy.deng@dingtone.me>
2019-03-12 12:35:29 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Logger
+## Installation
+### Gradle Dependency
+
+#####   Add the library to your project build.gradle
+
+```gradle
+compile 'com.joybar.logger:library:1.0.0'
+
+```
+
---
+++ library/src/main/java/com/joy/log/IPrinter.java
@@ -0,0 +1,42 @@
+package com.joy.log;
+
+/**
+ * Created by joybar on 2017/12/12.
+ */
+
+public interface IPrinter {
+
+	void v(Object object);
+	void v(String tag, Object object);
+
+	void v(String message);
+	void v(String tag, String message);
+
+	void i(Object object);
+	void i(String tag, Object object);
+	void i(String message);
+	void i(String tag, String message);
+
+	void d(Object object);
+	void d(String tag, Object object);
+	void d(String message);
+	void d(String tag, String message);
+
+
+	void w(Object object);
+	void w(String tag, Object object);
+	void w(String message);
+	void w(String tag, String message);
+
+
+	void e(Object object);
+	void e(String tag, Object object);
+	void e(String message);
+	void e(String tag, String message);
+
+	void json(String json);
+	void json(String tag, String json);
+
+	void xml(String xml);
+	void xml(String tag, String xml);
+}
